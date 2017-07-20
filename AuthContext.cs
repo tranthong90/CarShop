8747e37c84817698568409fc3e1ed950524f8513
tranthong90 <tom@softwareobjectives.com.au>
2017-07-20 18:46:24 +1000

    Initial commit

---
+++ AuthContext.cs
@@ -0,0 +1,87 @@
+﻿using CarShop.Model;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CarShop
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+        //: base("AuthContext")
+        {
+            Database.SetInitializer(new ApplicationDbInitializer());
+        }
+        public class ApplicationDbInitializer : DropCreateDatabaseAlways<AuthContext>
+        {
+            protected override void Seed(AuthContext context)
+            {
+                InitializeIdentityForEF(context);
+                base.Seed(context);
+            }
+
+            public static void InitializeIdentityForEF(AuthContext db)
+            {
+
+                if (!db.Users.Any())
+                {
+                    var roleStore = new RoleStore<IdentityRole>(db);
+                    var roleManager = new RoleManager<IdentityRole>(roleStore);
+                    var userStore = new UserStore<IdentityUser>(db);
+                    var userManager = new UserManager<IdentityUser>(userStore);
+
+                    // Add roles
+                    var adminRole = roleManager.FindByName("AdminGroup");
+                    if (adminRole == null)
+                    {
+                        adminRole = new IdentityRole("AdminGroup");
+                        roleManager.Create(adminRole);
+                    }
+
+                    var dealerRole = roleManager.FindByName("DealerGroup");
+                    if (dealerRole == null)
+                    {
+                        dealerRole = new IdentityRole("DealerGroup");
+                        roleManager.Create(dealerRole);
+                    }
+
+                    // Create admin users
+                    var adminUser = userManager.FindByName("Admin");
+                    if (adminUser == null)
+                    {
+
+                        adminUser = new IdentityUser
+                        {
+                            UserName = "Admin1",
+
+                        };
+
+                        userManager.Create(adminUser, "123456");
+                        userManager.SetLockoutEnabled(adminUser.Id, false);
+                        userManager.AddToRole(adminUser.Id, "AdminGroup");
+                    }
+
+                    var dealerUser = userManager.FindByName("Dealer");
+                    if (dealerUser == null)
+                    {
+
+                        dealerUser = new IdentityUser
+                        {
+                            UserName = "Dealer1",
+
+                        };
+
+                        userManager.Create(dealerUser, "123456");
+                        userManager.SetLockoutEnabled(dealerUser.Id, false);
+                        userManager.AddToRole(dealerUser.Id, "DealerGroup");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Mail;
+
+namespace CarShop
+{
+    public class Utility
+    {
+        public static void SendEmail(string toAddress, string subject, string body)
+        {
+            var fromAddress = new MailAddress("carsaletest123@gmail.com", "Tom Tran");
+            var toEmailAddress = new MailAddress(toAddress, "Carsale");
+            const string fromPassword = "carsale123456";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toEmailAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+    }
+}
